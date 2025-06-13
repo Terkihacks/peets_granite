3aea1667caca764cfe51662428de228b721b794b
Terkihacks <raymondmunguti163@gmail.com>
2025-06-13 15:11:17 +0300

    Updated Prisma and nodemon dependencies; refactored schema for improved clarity and consistency

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+ 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
